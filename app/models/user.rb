59f21f056e1812b3f6a652cd84076f9683ab0514
Alan Bradley <alanbradley@live.ie>
2015-10-25 12:01:23 +0000

    Added validations for users and listings

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  # validation for username not to be blank
+  validates :name, presence: true
 end
