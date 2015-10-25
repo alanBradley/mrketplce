18bd4fc63dd712138f3a4909c12c5f15cf02001b
Alan Bradley <alanbradley@live.ie>
2015-10-25 12:59:32 +0000

    Added seller page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -3,10 +3,16 @@ class ListingsController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create, :edit, :destroy]
   before_filter :check_user, only: [:edit, :update, :destroy]
 
+  # GET /seller
+  # Show listings which are by the current user
+  def seller
+    @listings = Listing.where(user: current_user).order("created_at DESC")
+  end
+
   # GET /listings
   # GET /listings.json
   def index
-    @listings = Listing.all
+    @listings = Listing.all.order("created_at DESC")
   end
 
   # GET /listings/1
