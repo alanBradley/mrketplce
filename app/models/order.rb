895bb01f92ef2468097662cf881eadc4d212265f
Alan Bradley <alanbradley@live.ie>
2015-10-25 19:09:15 +0000

    fixed user linking issue in orders model

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,7 @@ class Order < ActiveRecord::Base
 	validates :address, :city, :country, presence: true
 
 	belongs_to :listing
-	# class_name relates buyer and seller as a kind of user
-	belongs_to :buyer, class_name: "user"
-	belongs_to :seller, class_name: "user"
+	# class_name relates buyer and seller as a kind of User
+	belongs_to :buyer, class_name: "User"
+	belongs_to :seller, class_name: "User"
 end
