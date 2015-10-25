95e6301e4700d3d7b1857bc279d69fbc78da1a58
Alan Bradley <alanbradley@live.ie>
2015-10-25 11:29:52 +0000

    Added username for user and included strong parameters in applications controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151024235732) do
+ActiveRecord::Schema.define(version: 20151025111953) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20151024235732) do
     t.datetime "confirmation_sent_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
